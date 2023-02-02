82936a51c62c4ea038e5e6623a49119371630d76
suizoperuano <andrechedel@hotmail.com>
2023-02-02 18:51:33 +0000

    hey

--- src/app.js
+++ src/app.js
@@ -6,6 +6,9 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
   console.log("Hello Rigo from the console!");
 };
