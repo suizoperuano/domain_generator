772923655d4abab52b24d31bfa80c5cf1cdf5e62
suizoperuano <andrechedel@hotmail.com>
2023-02-02 19:39:23 +0000

    finished

--- src/app.js
+++ src/app.js
@@ -6,9 +6,15 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  let pronoun = ["the", "our"];
-  let adj = ["great", "big"];
-  let noun = ["jogger", "racoon"];
+  let pronouns = ["my", "our", "this"];
+  let adjectives = ["great", "big", "best"];
+  let nouns = ["cat", "racoon", "dicctionary"];
 
-  console.log("Hello Rigo from the console!");
+  for (let i = 0; i < pronouns.length; i++) {
+    for (let j = 0; j < adjectives.length; j++) {
+      for (let y = 0; y < nouns.length; y++) {
+        console.log(pronouns[i] + adjectives[j] + nouns[y] + ".com");
+      }
+    }
+  }
 };
